0698addaf14b4176cbe1bcdd4e7a4ecaee7b000d
Roy Meissner <meissner@informatik.uni-leipzig.de>
2017-12-01 16:16:18 +0100

    Added some scripts to preprocess system measurements

--- eval/extract_relevant_data.sh
+++ eval/extract_relevant_data.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-sadf -d test_X.bin -- -u > cpu.csv
-sadf -d test_X.bin -- -n > network.csv
-sadf -d test_X.bin -- -r > memory.csv
+if [ "$#" != "2" ]; then
+  echo "Too few/much arguments! Use like ./extract_relevant_data.sh INPUT_FILE_NAME IFACE_NAME"
+  echo "e.g. './extract_relevant_data.sh test_1.bin eth0'"
+  exit 1
+fi
+
+sadf -d $1 -- -u > cpu.csv
+sadf -d $1 -- -n DEV | grep "$2\|# " > network.csv
+sadf -d $1 -- -r > memory.csv
+
+#filter columns
+#filter for timestamp and idle
+cut --delimiter=";" -f 3,10 cpu.csv > cpu_filtered.csv
+#filter for timestamp, rxkB/s and txkB/s
+cut --delimiter=";" -f 3,7,8 network.csv > network_filtered.csv
+#filter for timestamp, kbmemfree and kbmemused
+cut --delimiter=";" -f 3,4,6 memory.csv > memory_filtered.csv
+
+#join files
+join --header -t\; cpu_filtered.csv network_filtered.csv | join --header -t\; - memory_filtered.csv > combined.csv
--- eval/measure_system.sh
+++ eval/measure_system.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sar -o test_X.bin 1
+if [ "$#" != "1" ]; then
+  echo "Too few/much arguments! Use like ''./measure_system.sh OUTPUT_FILE_EXTENSION'"
+  echo "e.g. './measure_system.sh 1' to get test_1.bin"
+  exit 1
+fi
+
+sar -o test_$1.bin 1
