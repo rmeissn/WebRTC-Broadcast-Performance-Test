c2bb5799accf655c6592e931eb856426d53de808
Roy Meissner <meissner@informatik.uni-leipzig.de>
2017-12-14 13:02:34 +0100

    improved time calculation

--- eval/combine_data.sh
+++ eval/combine_data.sh
@@ -16,8 +16,15 @@ while read p; do
     i=$((i + 1))
     continue
   fi
+  peers=$(echo $p | q-text-as-data --delimiter=";" "select c1 from -")
   start=$(echo $p | q-text-as-data --delimiter=";" "select c2 from -")
+  start=$(echo $start | cut -b1-10) #convert to seconds by round.floor
   end=$(echo $p | q-text-as-data --delimiter=";" "select c3 from -")
+  end=$(echo $end | cut -b1-10) #convert to seconds by round.floor
+  ((end = end + 1)) #add one to become round.ceil
+  delay=$(echo $p | q-text-as-data --delimiter=";" "select c4 from -")
+  variance=$(echo $p | q-text-as-data --delimiter=";" "select c5 from -")
+  deviation=$(echo $p | q-text-as-data --delimiter=";" "select c6 from -")
   idle=$(cat $1 | q-text-as-data --delimiter=";" -H "select avg(\"%idle\") from - where timestamp >= $start and timestamp <= $end")
   ((idle = 100 - idle))
   tx=$(cat $1 | q-text-as-data --delimiter=";" -H "select avg(\"txkb/s\") from - where timestamp >= $start and timestamp <= $end")
@@ -25,7 +32,7 @@ while read p; do
   memfree=$(cat $1 | q-text-as-data --delimiter=";" -H "select avg(\"kbmemfree\") from - where timestamp >= $start and timestamp <= $end")
   memused=$(cat $1 | q-text-as-data --delimiter=";" -H "select avg(\"kbmemused\") from - where timestamp >= $start and timestamp <= $end")
   i=$((i + 1))
-  echo "$p;$idle;$tx;$rx;$memfree;$memused"
+  echo "$peers;$start;$end;$delay;$variance;$deviation;$idle;$tx;$rx;$memfree;$memused"
 done < $2
 
 rm $1 $2
--- eval/time_conversion.awk
+++ eval/time_conversion.awk
@@ -3,7 +3,7 @@ BEGIN {
   OFS = ";";
 }
 {
-  if (NR != 1) command = "date -d ""\""$1"\""" +%s%3N";
+  if (NR != 1) command = "date -d ""\""$1"\""" +%s";
   if (NR != 1) command | getline $1;
   if (NR != 1) close(command);
   print;
